6bb00fa417e5dbbc22d15351467b256d8928b2aa
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-02 10:59:13 +0200

    DEEL_3

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//DEEL 2//
-
+//DEEL 3//
 
 'Use strict';
+
+const dishes = [{
+    id: 1,
+    name: 'Pasta Carbonara',
+    price: 12
+}, {
+    id: 2,
+    name: 'Scampies in creamsauce with tagliatteli',
+    price: 15
+
+}, {
+    id: 3,
+    name: 'Sausage with applesauce and potatoes',
+    price: 10
+}]
 
 window.onload = function () {
     console.log("rock & load");
@@ -9,12 +23,16 @@ window.onload = function () {
     document.getElementById('orderForm').addEventListener('submit', (event) => {
         event.preventDefault();
 
-        let name = document.getElementById("nameInput").value
+        let name = document.getElementById("nameInput").value;
         let email = document.getElementById("emailInput").value;
-        let order = document.getElementById("orderInput").value;
+        let order = document.querySelector('input[name="dish"]:checked').value;
+
+        let dish = dishes.find(d => d.id == order);
 
         let orderDetails = {
-            name,email,order
+            name,
+            email,
+            dish
         }
 
         printOrder(orderDetails);
@@ -22,6 +40,7 @@ window.onload = function () {
 }
 
 function printOrder(orderDetails) {
-    document.getElementById("messages").innerHTML = `<p>The order for the customer ${orderDetails.name}  is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.email}</p>`
+    document.getElementById("messages").innerHTML = `<p>The order for the customer ${orderDetails.name}  is the following:  ${orderDetails.dish.name}. The customer may be notified by email:  ${orderDetails.email}</p>`
 }
+
 
