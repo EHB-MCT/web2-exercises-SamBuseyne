f8bc7c3c81136dbbfb7a57589f28f09706d967b9
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-02 10:55:42 +0200

    DEEL_2

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-//DEEL 1//
+//DEEL 2//
+
 
 'Use strict';
 
 window.onload = function () {
     console.log("rock & load");
-        const form = document.getElementById('orderForm').addEventListener('submit', (event) => {
+
+    document.getElementById('orderForm').addEventListener('submit', (event) => {
         event.preventDefault();
-        let name = document.getElementById("nameInput").value;
+
+        let name = document.getElementById("nameInput").value
         let email = document.getElementById("emailInput").value;
         let order = document.getElementById("orderInput").value;
-        document.getElementById("messages").innerHTML = `<p>The order for the customer ${name}  is the following:  ${order}. The customer may be notified by email:  ${email}</p>`
+
+        let orderDetails = {
+            name,email,order
+        }
+
+        printOrder(orderDetails);
     });
 }
 
+function printOrder(orderDetails) {
+    document.getElementById("messages").innerHTML = `<p>The order for the customer ${orderDetails.name}  is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.email}</p>`
+}
 
