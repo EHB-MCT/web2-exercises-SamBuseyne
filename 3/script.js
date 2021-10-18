6b81acdc80542779c0a9ee6eb0775e4534767701
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-18 20:42:56 +0200

    Technical test

--- 3/script.js
+++ 3/script.js
@@ -1,34 +1,67 @@
 "Use strict";
 
+let counter = 0;
+const fetchUrl = "http://www.omdbapi.com/?s=&apikey=3d9f5461&"
 
 window.onload = function () {
-    async function getData(url){
-        let response = await fetch(url);
-        let data = await  response.json();
-        console.log(data);
-        document.getElementById("card-title").innerHTML = `${data.Title}`;
-        document.getElementById("card-text").innerHTML = `${data.Plot}`;
-        document.getElementById("Director").innerHTML = `${data.Director}`;
-        document.getElementById("Runtime").innerHTML = `${data.Runtime}`;
-        document.getElementById("Year").innerHTML = `${data.Year}`;
-        document.getElementById("image").innerHTML = `<img src="${data.Poster}" id="image" class="card-img" alt="...">`;
-        return data
 
-
-
+    async function getData(url) {
+        let data = await fetch(url);
+        return await data.json();
     }
+
+
     document.getElementById("button").addEventListener("click", e => {
         e.preventDefault();
-        let movieTitle = document.getElementById('inputTitle').value;
-        getData(`http://www.omdbapi.com/?t=${movieTitle}&apikey=3d9f5461&`)
+        let search = document.getElementById('inputTitle').value;
+        let year = document.getElementById("inputYear").value;
+        getData(`${fetchUrl}&t=${search}&y=${year}`)
+            .then(data => {
+                let movieCard = createMovieCard(data);
+                addCards(movieCard);
+            })
     })
 
-    function counter(){
-        document.getElementById("counterButton").addEventListener('click', event => {
-            console.log("check")
-        })
+    function createMovieCard(m) {
+        return `<div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4" id="image">
+                <img src="${m.Poster}"  class="card-img" alt="${m.Title}">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 id="card-title">${m.Title}</h5>
+                    <p id="card-text">${m.Plot}</p>
+                        <p id="Year">${m.Year}</p>
+                        <p id="Director">${m.Director}</p>
+                        <p id="Runtime">${m.Runtime}</p>
+                        <button type="button" id="addButton" runtime = "${m.Runtime}">+</button>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>`
 
+    document.getElementById("addButton").addEventListener('click', event => {
+        event.preventDefault();
+        console.log("check")
+        timeCounter(m.Runtime);
+    })
+    
+    }
+
+
+    function addCards(mC) {
+        document.getElementById("filmList").innerHTML += mC;
     }
 
 }
 
+
+
+
+function timeCounter(runtime) {
+    let time = parseInt(runtime);
+    runtime += time
+    document.getElementById("runtimeCounter").innerHTML = `${runtime} min`;
+}
---
+++ Projecten/script.js
@@ -0,0 +1,32 @@
+'Use strict';
+
+//General//
+
+window.onload = function () {
+    document.getElementById('OMDbButton').addEventListener('click', e => {
+        e.preventDefault('click');
+        let Title = document.getElementById('Title').value;
+        let year = document.getElementById("Year").value;
+        fetch(`http://www.omdbapi.com/?t=${Title}&y=${year}&apikey=3d9f5461&`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                let output = `<div>
+                <div>
+                    <div>
+                        <img src="${data.Poster}">
+                    </div>
+                    <div>
+                        <div>
+                            <h5>${data.Title}</h5>
+                            <p>${data.Plot}</p>
+                            <div><p>Duration: ${data.Runtime}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+                document.getElementById('loadingScreen').innerHTML = output;
+            })
+    })
+}
