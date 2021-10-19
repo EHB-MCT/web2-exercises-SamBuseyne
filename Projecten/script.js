c2fbad350198be23a7d49c1e8a9e21cc92a7cf17
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-19 07:50:25 +0200

    Technical test (update)
    Created ability to search by movie ID

--- Projecten/script.js
+++ Projecten/script.js
@@ -29,4 +29,37 @@ window.onload = function () {
                 document.getElementById('loadingScreen').innerHTML = output;
             })
     })
+
+    document.getElementById('IdSearch').addEventListener('click', e => {
+        e.preventDefault('click');
+        let Id = document.getElementById('ImdbId').value;
+        console.log(Id);
+        fetch(`http://www.omdbapi.com/?i=${Id}&apikey=3d9f5461&`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                let output = `<div>
+                <div>
+                    <div>
+                        <img src="${data.Poster}">
+                    </div>
+                    <div>
+                        <div>
+                            <h5>${data.Title}</h5>
+                            <p>${data.Plot}</p>
+                            <div><p>Duration: ${data.Runtime}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+                document.getElementById('loadingScreen').innerHTML = output;
+            })
+    })
+
+
+
+
+
+
 }
